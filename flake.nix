2e5b426c31742a3f22171b8b0d183e605098d700
Tom McLaughlin <tom@codedown.io>
2025-10-30 15:46:55 -0700

    Add haskell.nix build

--- flake.nix
+++ flake.nix
@@ -1,19 +1,50 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.gitignore = {
+    url = "github:hercules-ci/gitignore.nix";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+  inputs.haskellNix.url = "github:input-output-hk/haskell.nix/master";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, flake-utils, gitignore, haskellNix, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        compiler-nix-name = "ghc9122";
+
+        overlays = [
+          haskellNix.overlay
+
+          # Configure hixProject
+          (final: prev: {
+            hixProject = compiler-nix-name:
+              final.haskell-nix.hix.project {
+                src = gitignore.lib.gitignoreSource ./.;
+                evalSystem = system;
+                inherit compiler-nix-name;
+
+                modules = [{
+                  # Needed since GHC 9.10
+                  packages.unix.components.library.configureFlags = [''-f os-string''];
+                }];
+              };
+          })
+        ];
+
+        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+
       in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            zlib
-            haskell.compiler.ghc9122
-            websocat
-          ];
-        };
-      });
+        {
+          packages = ({
+            default = ((pkgs.hixProject compiler-nix-name).flake {}).packages."hi-profile-test:exe:hi-profile-test";
+          });
+
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              zlib
+              haskell.compiler.ghc9122
+              websocat
+            ];
+          };
+        });
 }
